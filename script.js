d716800c2d6cd5710c46646ecbb894aa37ca5d10
Ayush R Kumar <ayushrkumar2006@gmail.com>
2026-06-16 19:20:41 +0530

    Implement movement of elements on mouse move
    Add mousemove event listener to move elements

---
+++ script.js
@@ -0,0 +1,8 @@
+const nandi=document.getElementById("nandi")
+const garuda=document.getElementById("garuda")
+window.addEventListener("mousemove", (e) => {
+    const moveX = (e.clientX - window.innerWidth / 2) / 50;
+    const moveY = (e.clientY - window.innerHeight / 2) / 50;
+    nandi.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    garuda.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
