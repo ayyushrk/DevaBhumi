81c7a281951e7faa3a690f53a885a3d202eff8f3
Ayush R Kumar <ayushrkumar2006@gmail.com>
2026-06-16 19:28:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const nandi=document.getElementById("nandi")
 const garuda=document.getElementById("garuda")
 window.addEventListener("mousemove", (e) => {
+    const moveY = window.scrollY / 10;
     const moveX = (e.clientX - window.innerWidth / 2) / 50;
     const moveY = (e.clientY - window.innerHeight / 2) / 50;
     nandi.style.transform = `translate(${moveX}px, ${moveY}px)`;
